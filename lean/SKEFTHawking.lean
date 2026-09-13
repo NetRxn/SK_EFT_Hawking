2e05f917d84e8b3e5cd5ae07563a30598816dd75
NetRxn <NetRxn@proton.me>
2026-09-13 00:00:03 -0500

    Integrate multilevel process and transfer moments into aggregate

--- lean/SKEFTHawking.lean
+++ lean/SKEFTHawking.lean
@@ -1,3 +1,4 @@
+import SKEFTHawking.FiniteGibbsMultilevelProcess
 import SKEFTHawking.FiniteGibbsMultilevelReflection
 import SKEFTHawking.FiniteGibbsRelaxationProcess
 import SKEFTHawking.ClassicalFlowForcedCompact
